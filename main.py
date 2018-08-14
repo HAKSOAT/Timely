410860ce27e5ef21a8da3ef2b4411b4fd598d0dc
RealHaks <shopejuh@gmail.com>
2018-08-14 21:15:45 +0100

    The controller

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tki
+from alarm_ui import AlarmUI
+
+window = tki.Tk()
+
+window.minsize(600, 430)
+window.maxsize(600, 430)
+window.title("Timely")
+
+AlarmUI(window)
+
+window.mainloop()
