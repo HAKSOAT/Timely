5155b0aceaecc026344807d72dc47fa93a4e00b6
RealHaks <shopejuh@gmail.com>
2018-08-25 17:10:27 +0100

    Icons added

--- alarm_ui.py
+++ alarm_ui.py
@@ -19,22 +19,22 @@ class AlarmUI():
             self.storage = AlarmStorage("./")
             self.storage.create()
 
-        photo = tki.PhotoImage(file = "plus-16.gif")
+        photo = tki.PhotoImage(file = "icons/plus-16.gif")
         self.add_bt = tki.Button(self.master, text = "Add", image = photo, compound = "left", command = self.click_add)
         self.add_bt.image = photo
         self.add_bt.grid(row = 0, column = 0, sticky = tki.N + tki.E + tki.S + tki.W)
 
-        photo = tki.PhotoImage(file = "minus-16.gif")
+        photo = tki.PhotoImage(file = "icons/minus-16.gif")
         self.delete_bt = tki.Button(self.master, text = "Delete", image = photo, compound = "left", command = self.click_delete)
         self.delete_bt.image = photo
         self.delete_bt.grid(row = 0, column = 1, sticky = tki.N + tki.E + tki.S + tki.W)
 
-        photo = tki.PhotoImage(file = "edit-11-16.gif")
+        photo = tki.PhotoImage(file = "icons/edit-11-16.gif")
         self.edit_bt = tki.Button(self.master, text = "Edit", image = photo, compound = "left")
         self.edit_bt.image = photo
         self.edit_bt.grid(row = 0, column = 2, sticky = tki.N + tki.E + tki.S + tki.W)
 
-        photo = tki.PhotoImage(file = "copy-16.gif")
+        photo = tki.PhotoImage(file = "icons/copy-16.gif")
         self.clone_bt = tki.Button(self.master, text = "Clone", image = photo, compound = "left")
         self.clone_bt.image = photo
         self.clone_bt.grid(row = 0, column = 3, sticky = tki.N + tki.E + tki.S + tki.W)
