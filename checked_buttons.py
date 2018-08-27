8da4bb2d0f5a380e370dae33befe3d8bf4c9120f
RealHaks <shopejuh@gmail.com>
2018-08-27 18:09:24 +0100

    New widget labels checked

--- checked_buttons.py
+++ checked_buttons.py
@@ -5,8 +5,8 @@ class CheckedButtons():
     def __init__(self, checkbutton_states):
         self.checkbutton_states = checkbutton_states
 
-    def check_state(self):
-        states = []
-        for button, variable in self.checkbutton_states:
-            states.append(variable.get())
-        return states
+    def check_label_n_state(self):
+        labels_n_states = []
+        for button, variable, label in self.checkbutton_states:
+            labels_n_states.append([label, variable.get()])
+        return labels_n_states
