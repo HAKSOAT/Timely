e0a3ca453165096fbc2798a3ef0fd6e91a1d45cd
RealHaks <shopejuh@gmail.com>
2018-08-31 00:17:31 +0100

    Experiment implementing Popup.py

--- alarm_box.py
+++ alarm_box.py
@@ -1,4 +1,9 @@
 import tkinter as tki
+import threading
+from datetime import datetime as dt
+import time
+
+from popup import AlarmPopUp
 
 class AlarmBox():
 
@@ -17,6 +22,10 @@ class AlarmBox():
 
         self.alarm_canv["yscrollcommand"] = self.scrollbar.set
         self.checklabel_states = []
+
+        self.trigger_thread = None
+
+        self.create_thread()
 
     def show_alarm(self):
         self.checklabel_states = []
@@ -51,6 +60,44 @@ class AlarmBox():
         self.alarm_canv.create_window((checkbox_x + x, y), window = self.alarm_display, anchor = "n")
         self.alarm_canv.create_window((checkbox_x,checkbox_y + y),
                                        window = self.checkbutton, anchor = "n")
+
+    def create_thread(self):
+        self.trigger_thread = threading.Timer(1, self.trigger)
+        self.trigger_thread.start()
+
+    def trigger(self):
+        self.storage.connect()
+        db_result = self.storage.query()
+
+        ringtime = []
+
+        for each in db_result:
+            set_time = dt.now().replace(year = int(each[1]), month = int(each[2]), day = int(each[3]),
+                        hour = int(each[4]), minute = int(each[5]), second = 0)
+            time_left = set_time - dt.now()
+            ringtime.append((time_left.total_seconds(), each[6], [each[0], each[4], each[5]]))
+
+        ascending_ringtime = sorted(ringtime, key = lambda x: x[0])
+
+        for each in ascending_ringtime:
+            try:
+                time.sleep(int(each[0]))
+                self.popup = tki.Toplevel()
+                self.popup.transient(self.master)
+
+                self.popup.minsize(390, 350)
+                self.popup.maxsize(390, 350)
+
+                self.popup.title("Pop Up")
+                print(each[0])
+                AlarmPopUp(self.popup, self.storage, each[1], each[2][1], each[2][2])
+
+            except ValueError:
+                self.storage.connect()
+                self.storage.delete(each[2][0])
+                self.storage.commit()
+                self.storage.close()
+
 
     def delete(self):
         self.alarm_canv.delete("all")
