0b144011028f1072b91c4e848a296653a1665bbb
RealHaks <shopejuh@gmail.com>
2018-09-11 02:10:57 +0100

    App icon added

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ window.minsize(600, 395)
 window.maxsize(600, 395)
 window.title("Timely")
 
+img = tki.PhotoImage(file='icons/timely-icon.gif')
+window.tk.call('wm', 'iconphoto', window._w, img)
+
 AlarmUI(window)
 
 window.mainloop()
